0072c914a530f2cf77ac58b9645de17e9aecc882
AaronMathew11 <72664456+AaronMathew11@users.noreply.github.com>
2021-07-23 15:37:07 +0530

    initial commit

---
+++ taks 5/app/src/main/java/com/example/taks5/options.kt
@@ -0,0 +1,11 @@
+package com.example.taks5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class options : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_options)
+    }
+}
